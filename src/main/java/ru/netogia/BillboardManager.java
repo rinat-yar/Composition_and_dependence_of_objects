f84b7d244dd4d8fa76f1cae9f2f3f221b513ab1d
Rinat <rinat-yar@mail.ru>
2023-05-25 18:22:31 +0300

    Initial commit_6

--- src/main/java/ru/netogia/BillboardManager.java
+++ src/main/java/ru/netogia/BillboardManager.java
@@ -22,6 +22,7 @@ public class BillboardManager {
     }
 
     public String[] findAll() {
+
         return movies;
     }
 
--- src/test/java/ru/netogia/BillboarManager/BillboardManagerTest.java
+++ src/test/java/ru/netogia/BillboarManager/BillboardManagerTest.java
@@ -71,4 +71,50 @@ public class BillboardManagerTest {
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void testLimit10() {
+        BillboardManager manager = new  BillboardManager(10);
+
+        manager.addVMovie("Film 1");
+        manager.addVMovie("Film 2");
+        manager.addVMovie("Film 3");
+
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testLimit3() {
+        BillboardManager manager = new  BillboardManager(3);
+
+        manager.addVMovie("Film 1");
+        manager.addVMovie("Film 2");
+        manager.addVMovie("Film 3");
+        manager.addVMovie("Film 4");
+        manager.addVMovie("Film 5");
+
+        String[] expected = {"Film 5", "Film 4", "Film 3"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testFindLast4() {
+        BillboardManager manager = new BillboardManager(1);
+
+        manager.addVMovie("Film 1");
+        manager.addVMovie("Film 2");
+        manager.addVMovie("Film 3");
+        manager.addVMovie("Film 4");
+        manager.addVMovie("Film 5");
+
+        String[] expected = {"Film 5"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
